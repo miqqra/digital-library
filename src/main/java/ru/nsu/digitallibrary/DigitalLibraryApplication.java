4caa74ce2f248cc89f54f0edc9c69d0947424720
miqqra <mikhail.krasnikov.04@gmail.com>
2023-12-26 22:30:45 +0700

    Fix cors

--- src/main/java/ru/nsu/digitallibrary/DigitalLibraryApplication.java
+++ src/main/java/ru/nsu/digitallibrary/DigitalLibraryApplication.java
@@ -2,6 +2,9 @@ package ru.nsu.digitallibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DigitalLibraryApplication {
@@ -10,12 +13,14 @@ public class DigitalLibraryApplication {
         SpringApplication.run(DigitalLibraryApplication.class, args);
     }
 
-//    @Bean
-//    public Docket productApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("ru.nsu.digitallibrary.controller"))
-//                .build();
-//    }
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry reg) {
+                reg.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
