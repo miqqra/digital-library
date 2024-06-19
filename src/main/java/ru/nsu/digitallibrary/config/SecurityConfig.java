2615906df7c14b2bd60b1b54ecd02e6b86b97f50
miqqra <mikhail.krasnikov.04@gmail.com>
2024-06-19 22:52:13 +0700

    fix cors

--- src/main/java/ru/nsu/digitallibrary/config/SecurityConfig.java
+++ src/main/java/ru/nsu/digitallibrary/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         final var configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
