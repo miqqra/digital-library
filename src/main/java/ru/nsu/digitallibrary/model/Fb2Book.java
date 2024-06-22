980ebc18638ea4f76ae15231402acbcd358f6fd6
miqqra <mikhail.krasnikov.04@gmail.com>
2024-06-22 23:04:09 +0700

    fix npe on genres

--- src/main/java/ru/nsu/digitallibrary/model/Fb2Book.java
+++ src/main/java/ru/nsu/digitallibrary/model/Fb2Book.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -92,7 +93,9 @@ public class Fb2Book {
                 .map(TitleInfo::getGenres)
                 .stream()
                 .flatMap(Collection::stream)
+                .map(StringUtils::trim)
                 .map(Genres::getValueByGenreCode)
+                .filter(Objects::nonNull)
                 .map(Genres::getName)
                 .toList());
     }
